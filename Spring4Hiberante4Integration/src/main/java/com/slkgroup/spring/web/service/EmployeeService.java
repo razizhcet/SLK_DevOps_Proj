4fc334a8c13ccd44fb4c730bc65928773ff75ec9
imamsab.hulagur <imamsab.hulagur@slkgroup.com>
2019-04-26 10:47:55 +0530

    Adding project files

---
+++ Spring4Hiberante4Integration/src/main/java/com/slkgroup/spring/web/service/EmployeeService.java
@@ -0,0 +1,15 @@
+
+package com.slkgroup.spring.web.service;
+import java.util.List;
+
+import com.slkgroup.spring.web.entity.Employee;
+
+
+public interface EmployeeService {
+	public long createEmployee(Employee employee);
+    public Employee updateEmployee(Employee employee);
+    public void deleteEmployee(long id);
+    public List<Employee> getAllEmployees();
+    public Employee getEmployee(long id);	
+	public List<Employee> getAllEmployees(String employeeName);
+}
---
+++ Spring4Hiberante4Integration/src/test/java/com/slkgroup/spring/web/dao/impl/EmployeeDAOImplTest.java
@@ -0,0 +1,89 @@
+package com.slkgroup.spring.web.dao.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+import com.slkgroup.spring.web.dao.EmployeeDAO;
+import com.slkgroup.spring.web.entity.Employee;
+import com.slkgroup.spring.web.service.EmployeeService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath:/mvc-dispatcher-servlet.xml" })
+@PropertySource({ "classpath:/application.properties" })
+public class EmployeeDAOImplTest {
+
+	@Autowired
+	private EmployeeService employeeService;
+
+	@Autowired
+	private EmployeeDAO employeeDAO;
+
+	Employee employee = new Employee();
+
+	@Test
+	public void testingDAOObject() {
+		System.out.println(employeeDAO.hashCode());
+	}
+
+	@Before
+	public void setUpBeforeEachTest() {
+
+		employee.setId(11);
+		employee.setName("Smith");
+		employee.setAge(30);
+		employee.setSalary(12000);
+	}
+
+	@Test
+	public void testCreateEmployee() {
+		employeeDAO.createEmployee(employee);
+		assertEquals("Smith", employee.getName());
+	}
+
+	@Test
+	public void testUpdateEmployee() {
+		employeeDAO.createEmployee(employee);
+		employee.setName("baby");
+		employeeDAO.updateEmployee(employee);
+		assertEquals("baby", employee.getName());
+	}
+
+	@Test
+	public void testDeleteEmployee() {
+
+		employeeDAO.createEmployee(employee);
+		int size = employeeDAO.getAllEmployees().size();
+		employeeDAO.deleteEmployee(employee.getId());
+		assertTrue(size > employeeDAO.getAllEmployees().size());
+	}
+
+	@Test
+	public void testGetEmployee() {
+		employeeDAO.createEmployee(employee);
+		employeeDAO.getEmployee(employee.getId());
+		assertEquals("Smith", employee.getName());
+	}
+
+	@Test
+	public void testGetAllEmployees() {
+		employeeDAO.createEmployee(employee);
+		employeeDAO.getAllEmployees(employee.getName());
+		assertEquals("Smith", employee.getName());
+	}
+
+	@Test
+	public void testGetAllEmployeesString() {
+		employeeDAO.createEmployee(employee);
+		employeeDAO.getAllEmployees(employee.getName());
+		assertEquals("Smith", employee.getName());
+	}
+
+}
